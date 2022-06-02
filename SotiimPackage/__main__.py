acab0062b630cc2a666360014e0c43efd3b27c70
Cesar Alberto Garcia Aranda <csrrnld@gmail.com>
2022-06-01 22:41:38 -0500

    Update

--- SotiimPackage/__main__.py
+++ SotiimPackage/__main__.py
@@ -1,7 +1,9 @@
+import logging
 from SotiimPackage import unreleased
 
 
+logging.basicConfig(level=logging.DEBUG)
 if __name__ == "__main__":
     workshops= unreleased()
-    print(workshops)
+    logging.debug(unreleased.__doc__)
 
--- SotiimPackage/workshops.py
+++ SotiimPackage/workshops.py
@@ -4,6 +4,8 @@ from urllib import request, response
 import requests
 
 def unreleased():
+    """Retorna Data de una Api
+    """
     response = requests.get('https://codigofacilito.com/api/v2/workshops/unreleased')
 
     if response.status_code == 200:
